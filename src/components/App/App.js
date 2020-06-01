9655df1d28cbad3af2ae46f7f384dd556db698fe
mjesiolowski <maciej.jesiolowski@gmail.com>
2020-06-01 15:22:26 +0200

    connected rate with input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const App = () => (
-  <>
-    <h1>TEST App.js</h1>
-    <div>
-      <form>
-        <input type="number" />
-        <button>Setup rate</button>
-      </form>
-    </div>
-  </>
-);
+export const App = ({
+  currencyRate,
+  setRate,
+}) => {
+  const [rateInput, setRateInput] = useState(currencyRate);
+
+  const handleSetRate = (e) => {
+    const { value } = e.target;
+
+    setRateInput(value);
+    setRate({ rate: parseInt(value, 10) });
+  };
+
+  return (
+    <>
+      <h1>TEST App.js</h1>
+      <div>
+        <form>
+          <input
+            type="number"
+            value={rateInput}
+            onChange={(e) => handleSetRate(e)}
+          />
+        </form>
+      </div>
+    </>
+  );
+};
--- src/components/App/connect.js
+++ src/components/App/connect.js
@@ -2,7 +2,13 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { App } from './App';
 import { getCurrencyRate } from '../../selectors';
+import { setRate } from '../../actions';
+
+const mapDispatchToProps = (dispatch) => ({
+  setRate: (rate) => dispatch(setRate(rate)),
+});
 
 export default connect(
   createStructuredSelector({ currencyRate: getCurrencyRate }),
+  mapDispatchToProps,
 )(App);
