b69e1295c3e3b6880bc1c0bddfc7b84ac6ead794
mjesiolowski <maciej.jesiolowski@gmail.com>
2020-06-01 15:07:10 +0200

    added rate input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export const App = () => (
-  <h1>TEST App.js</h1>
+  <>
+    <h1>TEST App.js</h1>
+    <div>
+      <form>
+        <input type="number" />
+        <button>Setup rate</button>
+      </form>
+    </div>
+  </>
 );
